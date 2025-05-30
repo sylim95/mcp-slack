c7c69e54f18dab9ece9039fe6169c819effdc058
sylim95 <100990677+sylim95@users.noreply.github.com>
2025-05-30 11:59:38 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
