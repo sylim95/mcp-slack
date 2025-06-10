ce72e9e91ab55ca889d88e33844787dcb99f9337
sylim95 <100990677+sylim95@users.noreply.github.com>
2025-06-10 11:04:57 +0900

    Update server.js - Jetbrain #3

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 app.use(cors());
